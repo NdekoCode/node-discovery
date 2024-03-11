0280eec09eb51406ede4bf7d71056351c4596fd9
Ndekocode <arickbulakali@gmail.com>
2024-03-11 17:16:05 +0200

    docs: import local module

---
+++ 02/use-local-module.js
@@ -0,0 +1,6 @@
+// function (exports, module, require, __filename, __dirname){
+const api = require('../01/1-exports-func');
+const getFullname = api.getFullname
+console.log(getFullname('Arick',"Bulakali"));
+// return module.exports;
+// } 
